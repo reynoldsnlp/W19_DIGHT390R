7b320bd20099b6b6ec7be75a938c125a401b0bfd
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2019-01-28 15:48:41 -0700

    alternative test script

---
+++ RBMT/apertium_HOWTO/tests2.sh
@@ -0,0 +1,5 @@
+cat test_sents.txt | while read sent
+do
+	echo -e ${sent}$"\t\t"$(echo ${sent} | lt-proc hbs-eng.automorf.bin | \
+	gawk 'BEGIN{RS="$"; FS="/";}{nf=split($1,COMPONENTS,"^"); for(i = 1; i<nf; i++) printf COMPONENTS[i]; if($2 != "") printf("^%s$",$2);}' |   apertium-transfer apertium-hbs-eng.hbs-eng.t1x hbs-eng.t1x.bin hbs-eng.autobil.bin  |   lt-proc -g hbs-eng.autogen.bin)
+done
